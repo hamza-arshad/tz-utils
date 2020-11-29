9933bd3c73f0dc56b83d85d4421e690cecc5841d
inker <anton.veselyev@gmail.com>
2020-11-29 02:06:56 +0000

    update offset functions

--- src/offset.ts
+++ src/offset.ts
@@ -1,4 +1,7 @@
 import moment from 'moment-timezone'
 
-export default (timezone: string) =>
-  moment.tz(timezone).utcOffset()
+export const configurable = (getRelativeDate: () => Date) =>
+  (timezone: string) =>
+    moment(getRelativeDate()).tz(timezone).utcOffset()
+
+export default configurable(() => new Date())
