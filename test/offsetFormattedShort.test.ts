d68be696941f11264ff3245e74418b3204d0f42f
inker <anton.veselyev@gmail.com>
2020-11-29 01:07:46 +0000

    update offset tests

--- test/offsetFormattedShort.test.ts
+++ test/offsetFormattedShort.test.ts
@@ -2,47 +2,31 @@ import {
   names as timezoneNames,
   deprecatedNames,
   notWorkingInDateFnsNames,
-  offsetFormattedShort,
 } from '../src'
 
-const RE = /^GMT([+-]\d{1,2}(:\d{2})?)?$/
-const THIRTY_MINUTES_RE = /^GMT[+-]\d:30$/
+import { configurable } from '../src/offsetFormattedShort'
 
-const rubbishTimezone = 'shit'
+import dates from './data/dates'
 
-const allNames = [
+const timezones = [
   ...timezoneNames,
   ...deprecatedNames,
   ...notWorkingInDateFnsNames,
 ]
 
 describe('offsetFormattedShort', () => {
-  for (const tz of allNames) {
-    it(`should output correct result for ${tz}`, () => {
-      const result = offsetFormattedShort(tz)
-      expect(result).toMatch(RE)
-      expect(result).not.toMatch(/\:00$/)
+  for (const date of dates) {
+    const offsetFormattedShort = configurable(() => new Date(date))
+    for (const tz of timezones) {
+      it(`should output correct result for (${date}, ${tz})`, () => {
+        const result = offsetFormattedShort(tz)
+        expect(result).toMatchSnapshot()
+      })
+    }
+
+    it(`should work correctly with rubbish data (${date})`, () => {
+      const result = offsetFormattedShort('shit')
+      expect(result).toBe(null)
     })
   }
-
-  it('should output correct sign', () => {
-    const ny = offsetFormattedShort('America/New_York')!
-    const moscow = offsetFormattedShort('Europe/Moscow')!
-    const kolkata = offsetFormattedShort('Asia/Kolkata')!
-    const gmt = offsetFormattedShort('GMT')
-    expect(ny[3]).toBe('-')
-    expect(moscow[3]).toBe('+')
-    expect(kolkata[3]).toBe('+')
-    expect(gmt).toBe('GMT')
-  })
-
-  it('should output minutes for fractional timezones', () => {
-    const kolkata = offsetFormattedShort('Asia/Kolkata')
-    expect(kolkata).toMatch(THIRTY_MINUTES_RE)
-  })
-
-  it('should work correctly with rubbish data', () => {
-    const result = offsetFormattedShort(rubbishTimezone)
-    expect(result).toBe(null)
-  })
 })
